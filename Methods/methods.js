b10857d1ed26493bdde5db6f88bf02c6c2b453df
Deyvessh <deyvesshkumar@gmail.com>
2018-08-09 19:51:11 +0530

    basic concepts, methods

---
+++ Methods/methods.js
@@ -0,0 +1,21 @@
+
+
+// adding class to h1
+
+$("h1").addClass("correct");
+
+// adding class to list
+
+$("li").addClass("wrong");
+
+// removing existing class to last li
+
+$("li").last().removeClass("wrong");
+
+// adding addClass to h2
+
+$("h2").addClass("done");
+
+// toggleClass on h2 if class is active then it will toggle it inactive.
+
+$("h2").toggleClass("sdone");
---
+++ index.js
@@ -0,0 +1,22 @@
+ 
+// Makes all div of purple color
+
+$("div").css("background", "purple");
+
+// Makes class .hightlight div 60px wide font-size
+
+$(".highlight").css("font-size","60px");
+
+
+// Makes class .highlight div width 200px.
+
+$(".highlight").css("width","200px");
+
+
+// Makes id #third div orange border
+
+$("#third").css("border"," 2px solid orange");
+
+// Makes only first div to white color.
+
+$("div:first-of-type").css("color","white");
