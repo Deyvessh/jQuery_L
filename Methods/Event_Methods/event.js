08b732e2188f7897dac384aa2683507ad07f295d
Deyvessh <deyvesshkumar@gmail.com>
2018-08-10 17:28:30 +0530

    jQuery Event Methods

---
+++ Methods/Event_Methods/event.js
@@ -0,0 +1,11 @@
+// ------------------
+// ----------on()--
+// ---------------------
+
+
+$("#div1").on("click", function() {
+    $(".divBox").fadeIn(1000, function() {
+        // alert("This is Fadeout Property");
+    });
+    // $(".divBox").remove();
+});
